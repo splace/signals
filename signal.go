23c1379a41524c0b2986e35ed8906de81477c586
simon <psiplace@netscape.net>
2016-02-11 01:11:42 +0000

    spelling

--- signal.go
+++ signal.go
@@ -3,7 +3,7 @@
 // where they "convey information about the behavior or attributes of some phenomenon"
 // here, more specifically, "any quantity exhibiting variation in time or variation in space"
 // currently this package supports only 1-Dimensionsal variation.
-// and for simplicity terminolology used represents analogy variation in time.
+// and for simplicity terminolology used represents analogue variation in time.
 // this package is intended to be general, and so a base package for import, and used then with specific real-world quantities.
 package signals
 
