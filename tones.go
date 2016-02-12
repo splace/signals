d3a77009de49824ff9607de6ce2d334f1e6692df
simon <psiplace@netscape.net>
2016-02-12 00:40:45 +0000

    comments no-break

---
+++ tones.go
@@ -0,0 +1,16 @@
+package signals
+
+// Tones are signals that repeat periodically
+type Tone interface{
+	Signal
+	Period() Interval
+}
+
+// Tone whose source is a scaled sine
+func NewTone(period Interval, Volume uint8) Tone {
+	return Product{Sine{period}, Constant{MaxLevel / 100 * Level(Volume)}}
+}
+
+
+
+
