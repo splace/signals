688a88eb75bf96c3eb21e06c90dce36b2cca11a6
simon <psiplace@netscape.net>
2016-02-13 00:48:20 +0000

    improv

--- doc.go
+++ doc.go
@@ -1,26 +1,24 @@
 /*
+Overview
+
 Package signals generates and manipulates signals:- https://en.wikipedia.org/wiki/Signal_processing.
 
 signals are entirely procedural.
 
-signals "any quantity exhibiting variation in time or variation in space".
-
-currently this package supports only 1-Dimensionsal variation, and for simplicity terminolology used represents analogue variation in time.
+currently this package supports only 1-Dimensionsal variation, and for simplicity the terminolology used represents analogue variation in time.
 
 this package is intended to be general, and so a base package for import, and used then with specific real-world quantities.
 
+Fundamental Types
+
+Level :- can have a value from -MaxLevel to +MaxLevel
+Interval :- can generally be used as if infinite, with UnitTime somewhere near the center of its precision range.
+
 Interfaces
 
-Signal :- method Level() returns a Level value from an Interval value parameter.
+Signal :- has method Level() which returns a Level value from an Interval value parameter.
 Tone :- a Signal with an additional method  Period(), that returns the signals repeat period Interval.
 	
-Main Types
-
-Level :- a value from -MaxLevel to +MaxLevel
-Interval :- a value with UnitTime somewhere near the center of its range.
-
-
-
 */
 package signals
 
