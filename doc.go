856a4533b18bcb14214a135a177f54b8d46af19e
simon <psiplace@netscape.net>
2016-02-13 00:52:16 +0000

    improv

--- doc.go
+++ doc.go
@@ -1,18 +1,18 @@
 /*
-Overview
-
 Package signals generates and manipulates signals:- https://en.wikipedia.org/wiki/Signal_processing.
 
-signals are entirely procedural.
+Overview
 
-currently this package supports only 1-Dimensionsal variation, and for simplicity the terminolology used represents analogue variation in time.
+signals are entirely procedural, all values are calculated as needed, currently no cache.
 
-this package is intended to be general, and so a base package for import, and used then with specific real-world quantities.
+this package supports only 1-Dimensionsal variation, and for simplicity the terminolology used represents analogue variation in time.
+
+this package is intended to be general, and so a base package for import, and then used with specific real-world quantities.
 
 Fundamental Types
 
 Level :- can have a value from -MaxLevel to +MaxLevel
-Interval :- can generally be used as if infinite, with UnitTime somewhere near the center of its precision range.
+Interval :- 'usually' can be used as if infinite (+ve and -ve), with UnitTime somewhere near the center of its precision range.
 
 Interfaces
 
