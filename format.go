42e3bf60c846b68b25b1291b67c39fbb75488bef
simon <psiplace@netscape.net>
2016-02-16 00:17:25 +0000

    32 bit

--- format.go
+++ format.go
@@ -2,15 +2,12 @@ package signals
 
 import (
 	"encoding/binary"
-	//"bytes"
+	"bytes"
 	"fmt"
 	"io"
 )
 
-
-
-
-// encode a signal as an unsigned PCM data in a Riff wave container (wav file format) 
+// encode a signal as PCM data in a Riff wave container (mono wav file format) 
 func Encode(w io.Writer, s Signal, length interval, sampleRate uint32, sampleBytes uint8) {
 	binaryWrite := func(w io.Writer, d interface{}) {
 		if err := binary.Write(w, binary.LittleEndian, d); err != nil {
@@ -32,63 +29,28 @@ func Encode(w io.Writer, s Signal, length interval, sampleRate uint32, sampleByt
 	binaryWrite(w, uint16(8*sampleBytes))
 	fmt.Fprint(w, "data")
 	binaryWrite(w, samples*uint32(sampleBytes))
-	var shift uint8 = LevelBits - 8*sampleBytes
+	var i uint32
 	switch sampleBytes{
 	case 1:
-		var i uint32
 		for ; i < samples; i++ {
-			l:=s.Level(interval(i)*samplePeriod)
-			binaryWrite(w,uint8(l>>shift+128))
+			binaryWrite(w,uint8(s.Level(interval(i)*samplePeriod)>>(LevelBits - 8)+128))
 		}
 	case 2:
-		var i uint32
 		for ; i < samples; i++ {
-			l:=s.Level(interval(i)*samplePeriod)
-			binaryWrite(w,int16(l>>shift))
+			binaryWrite(w,int16(s.Level(interval(i)*samplePeriod)>>(LevelBits - 16)))
 		}
-/*	case 3:
-		var i uint32
-		buf:= bytes.NewBuffer(make([]bytes,4))
+	case 3:
+		buf:= bytes.NewBuffer(make([]byte,4))
 		for ; i < samples; i++ {
-			l:=s.Level(interval(i)*samplePeriod)
-			binaryWrite(buf,int32(l>>shift))
+			binaryWrite(buf,int32(s.Level(interval(i)*samplePeriod)>>(LevelBits - 32)))
+			w.Write(buf.Bytes()[1:])
 		}
-*/
+
 	case 4:
-		var i uint32
 		for ; i < samples; i++ {
-			l:=s.Level(interval(i)*samplePeriod)
-			binaryWrite(w,int32(l>>shift))
+			binaryWrite(w,int32(s.Level(interval(i)*samplePeriod)>>(LevelBits - 32)))
 		}
 	}	
 }
 
-
-
-/* PCM all possible formats
-
-openal takes s16 or u8
-
- DE alaw            PCM A-law
- DE f32be           PCM 32-bit floating-point big-endian
- DE f32le           PCM 32-bit floating-point little-endian
- DE f64be           PCM 64-bit floating-point big-endian
- DE f64le           PCM 64-bit floating-point little-endian
- DE mulaw           PCM mu-law
- DE s16be           PCM signed 16-bit big-endian
- DE s16le           PCM signed 16-bit little-endian
- DE s24be           PCM signed 24-bit big-endian
- DE s24le           PCM signed 24-bit little-endian
- DE s32be           PCM signed 32-bit big-endian
- DE s32le           PCM signed 32-bit little-endian
- DE s8              PCM signed 8-bit
- DE u16be           PCM unsigned 16-bit big-endian
- DE u16le           PCM unsigned 16-bit little-endian
- DE u24be           PCM unsigned 24-bit big-endian
- DE u24le           PCM unsigned 24-bit little-endian
- DE u32be           PCM unsigned 32-bit big-endian
- DE u32le           PCM unsigned 32-bit little-endian
- DE u8              PCM unsigned 8-bit
-
-*/
 
