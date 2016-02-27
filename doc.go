b7243e852868c8bcb3ac4d4f8b00458663bfb56f
splace <simon.place@googlemail.com>
2016-02-27 06:11:51 +0000

    improve doc

--- doc.go
+++ doc.go
@@ -5,13 +5,19 @@ Overview
 
 intended to be abstract, and a base package for import, then used with specific real-world quantities.
 
-Functions are generally procedural, calculated as needed, meaning changes in parameters, or arrangment, effective returned values of existing types.
+Interfaces
 
-PCMFunctions are stored, at a particular interval and precision, and can be used to cache an expensive precedural Function.
+Function :- has method Call() which returns a 'y' value from an 'x' value parameter.
+Function's are generally procedural, calculated as needed, meaning changes in parameters, or arrangment, effect returned values of existing types.
+Function's can be encode/decoded as go code binary (gob), making for a basic interpreted signal language, or they can be, lossily, stored in wav files (Function's saved as wav are loaded back as PCMFunctions)
 
-Functions can be encode/decoded as go code binary (gob), making for a basic interpreted signal language.
+LimitedFunction :- has a MaxX() method that returns the 'x' value after which the function can be assumed to return zero, effectively has an end.
 
-or they can, lossily, be stored in wav files (functions saved as wav are loaded back as PCMFunctions)
+PeriodicFunction :- a Function with an additional method Period(), reciprocal of any fundamental frequency, (or sample spacing for PCMFunction's), delta 'x'.
+
+PCMFunction :- a PeriodicLimitedFunction with additional method Encode().
+PCMFunction's are stored, at a particular interval and precision, and can be used to cache an expensive precedural Function.
+
 
 Fundamental Types
 
@@ -19,16 +25,6 @@ x :- 'usually' can be used as if infinite (+ve and -ve), with UnitX somewhere ne
 
 y :- can have a value from -Maxy to +Maxy
 
-Interfaces
-
-Function :- has method Call() which returns a y value from an x value parameter.
-
-LimitedFunction :- has a MaxX() method that returns the x value after which the function can be assumed to return zero. ie ends.
-
-PeriodicFunction :- a Function with an additional method Period(), reciprocal of any fundamental frequency, (or sampling for PCM), delta x.
-
-PCMFunction :- a PeriodicLimitedFunction with additional method Encode().
-(NB Period() for PCMFunctions represents the sample spacing, since repeating stored functions are assumed not required, use a Looped modifier.)
-
+Note: x and y are package local to allow changes to how they are represented internally to be flexible, some access is provided through functions. 
 */
 package signals
