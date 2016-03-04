42888c7edd95505a8621ac6c87eece128b9d7697
simon <psiplace@netscape.net>
2016-03-04 23:18:36 +0000

    fix for X()

--- examples/generatewav.go
+++ examples/generatewav.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	m := NewTone(UnitX/100, -6)
+	m := NewTone(X(1.0/100), -6)
 	file, err := os.Create(fmt.Sprintf("Sine%+v.wav", m)) // file named after go code of generating Function
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
-	Encode(file, m, 1*UnitX, 8000, 2)
+	Encode(file, m, X(1), 8000, 2)
 }
 
