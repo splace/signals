050c03b6c0a2c896e60da8bd7d4fc4d41026dba5
simon <psiplace@netscape.net>
2016-04-29 00:39:20 +0100

    data>Data

--- examples/player/play.go
+++ examples/player/play.go
@@ -14,7 +14,7 @@ func play(s Function) {
 	cmd := exec.Command("aplay","-f","S16","-r","44100")
 	out, in := io.Pipe()
 	go func() {
-		io.Copy(in, bytes.NewReader(NewPCMFunction(s, OneSecond*3, 44100, 2).(PCM16bit).PCM.data))
+		io.Copy(in, bytes.NewReader(NewPCMFunction(s, OneSecond*3, 44100, 2).(PCM16bit).PCM.Data))
 		in.Close()
 	}()
 	cmd.Stdin=out 
@@ -28,9 +28,4 @@ func main(){
 	play(Looped{Modulated{Pulse{OneSecond}, Looped{Pulse{OneSecond*4/10}, OneSecond*6/10}, Stack{Sine{OneSecond/450},Sine{OneSecond/400}}}, OneSecond*3})
 }
 
-
-/*  Hal3 Fri Apr 29 00:28:36 BST 2016 go version go1.5.1 linux/amd64
-Fri Apr 29 00:28:36 BST 2016 */
-/*  Hal3 Fri Apr 29 00:31:06 BST 2016 go version go1.5.1 linux/amd64
-Fri Apr 29 00:31:06 BST 2016 */
 
