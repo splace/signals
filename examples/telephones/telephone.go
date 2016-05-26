8dab99ab617b6af6d9d39379088058dea470963a
simon <psiplace@netscape.net>
2016-05-26 18:50:56 +0100

    examples Synopsis

--- examples/telephones/telephone.go
+++ examples/telephones/telephone.go
@@ -1,5 +1,5 @@
-// generate a few standard telephone notification tones.
-// length is a multiple of the repeat cycle, so play output repeatedly to get any length. 
+// generate a few standard telephone notification tones into wav files.
+// tone duration is a multiple of the repeat cycle, so to get any length play output repeatedly. 
 package main
 
 import (
