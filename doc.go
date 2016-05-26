69528d019b4d3040bebe67a9cfe8cbabf32c3728
simon <psiplace@netscape.net>
2016-05-26 18:42:31 +0100

    improve

--- doc.go
+++ doc.go
@@ -1,10 +1,10 @@
 /*
-Package signals generates and manipulates abstract signals, when imported can then be used with specific real-world quantities.
+Package signals generates, stores and manipulates abstract signals, when its imported it can then be used with specific real-world quantities.
 
 
 Definition of signal
 
-A signal is the value of some property, as it depends, uniquely, on some parameter.
+A signal is the varying value of some property, as it depends, uniquely, on some parameter.
 
 The controlling parameter is generally unbounded, and the property bounded.
 
@@ -22,7 +22,7 @@ Interfaces
 
 Signal :- has method property(x), which returns a 'y' value from an 'x' value parameter.
 Signal's are generally procedural, calculated as needed, meaning changes in parameters, or arrangement, effect returned values of existing types.
-Signal's can be saved/loaded from go code binary (gob), making for a basic interpreted signal language, or they can be stored, lossily, in wav files (Signal's saved as wav are loaded back as PCMSignals)
+Signal's can be saved/loaded from a go code binary (gob) file, making for a basic interpreted signal language, or they can be stored, lossily, as PCM data. (PCM data can be encoded and saved in a Waveform Audio File Format (wav) file.)
 
 LimitedSignal :- has a MaxX() method that returns the 'x' value after which the Signal can be assumed to return zero, effectively has an end.
 an 'x' value of zero is normally regarded as its start.
@@ -42,5 +42,6 @@ package signals
 Implementation details.
 
 x and y are not exported, separating their abstract nature from an importing packages concrete implementation and allowing flexibility in representation, if needed they can be made through provided exposed functions.
-x and y are encoded as fixed precision, so resolution doesn't vary with value.
+x and y are encoded as non-floating types, so resolution doesn't vary with value, but by changing unitX the precision of a value can be changed, and the overall range can be altered, making for a basic ability to 'float' the range of the variable, but only when directed, not automatically.
+
 */
