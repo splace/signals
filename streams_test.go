c6756217067c9ec7e251fe14669f65afd45df8cb
simon <psiplace@netscape.net>
2016-06-22 00:08:02 +0100

    fade in stream test

--- streams_test.go
+++ streams_test.go
@@ -7,6 +7,27 @@ import (
 	"net/url"
 )
 
+func TestStreamsRemoteSave(t *testing.T) {
+	s,err:=NewWave("http://www.nch.com.au/acm/8k8bitpcm.wav")
+	if err!=nil{
+		if ue,ok:=err.(*url.Error);ok {
+			if oe,ok:=ue.Err.(*net.OpError);ok{
+				if se,ok:=oe.Err.(*os.SyscallError);ok{
+					if se.Err.Error()=="connection refused"{
+						t.Skip(se.Err.Error())
+					}
+				}
+			}
+		}
+		t.Fatal(err)
+	}
+	t.Logf("%v\n",s)
+	file, err := os.Create("./test output/remoteStream.wav")
+	if err != nil {panic(err)}
+	defer file.Close()
+	Encode(file, 1, 8000, unitX*3, s)
+}
+
 func TestStreamsLocalSave(t *testing.T) {
 	s,err:=NewWave("http://localhost:8086/wavs/s16/4.wav?f=8000")
 	if err!=nil{
@@ -28,8 +49,9 @@ func TestStreamsLocalSave(t *testing.T) {
 	Encode(file, 1, 8000, unitX*3, s)
 }
 
-func TestStreamsRemoteSave(t *testing.T) {
-	s,err:=NewWave("http://www.nch.com.au/acm/8k8bitpcm.wav")
+
+func TestStreamsLocalRampUpSave(t *testing.T) {
+	s,err:=NewWave("http://localhost:8086/wavs/s16/4.wav?f=8000")
 	if err!=nil{
 		if ue,ok:=err.(*url.Error);ok {
 			if oe,ok:=ue.Err.(*net.OpError);ok{
@@ -43,10 +65,11 @@ func TestStreamsRemoteSave(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("%v\n",s)
-	file, err := os.Create("./test output/remoteStream.wav")
+	fs:=Modulated{s,RampUp{unitX}}
+	file, err := os.Create("./test output/localFadeInStream.wav")
 	if err != nil {panic(err)}
 	defer file.Close()
-	Encode(file, 1, 8000, unitX*3, s)
+	Encode(file, 1, 8000, unitX*3, fs)
 }
 
 
