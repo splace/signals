5e54ee4e94a0159f8e7b92bee1240ff4e0ef58b0
simon <psiplace@netscape.net>
2016-06-24 23:14:24 +0100

    comment

--- image.go
+++ image.go
@@ -6,17 +6,17 @@ import (
 	"image/color/palette"
 )
 
-// Depiction of a Signal, implements Depictor
+// simple visual Depiction of a Signal, implements Depictor
 type Depiction struct {
 	Signal
 	size                     image.Rectangle
 	pixelsPerUnitX           int
-	belowColour, aboveColour color.Color
+	below, above color.Color
 }
 
-// makes a Depiction of a LimitedSignal, scaled to pxMaxx x pxMaxy pixels and sets its colours.
-func NewDepiction(s LimitedSignal, pxMaxX, pxMaxY int, c1, c2 color.Color) Depiction {
-	return Depiction{s, image.Rect(0, -pxMaxY/2, pxMaxX, pxMaxY/2), int(int64(pxMaxX) * int64(unitX) / int64(s.MaxX())), c1, c2}
+// makes a Depiction of a LimitedSignal, scaled to pxMaxx by pxMaxy pixels and sets the colours for above and below the value.
+func NewDepiction(s LimitedSignal, pxMaxX, pxMaxY int, below, above color.Color) Depiction {
+	return Depiction{s, image.Rect(0, -pxMaxY/2, pxMaxX, pxMaxY/2), int(int64(pxMaxX) * int64(unitX) / int64(s.MaxX())), below, above}
 }
 
 func (i Depiction) Bounds() image.Rectangle {
@@ -25,9 +25,9 @@ func (i Depiction) Bounds() image.Rectangle {
 
 func (i Depiction) At(xp, yp int) color.Color {
 	if i.property(x(xp)*unitX/x(i.pixelsPerUnitX)-x(i.size.Min.X)) <= unitY/y(i.size.Max.Y)*y(yp)-y(i.size.Min.Y) {
-		return i.aboveColour
+		return i.above
 	}
-	return i.belowColour
+	return i.below
 }
 
 // a Depictor is an image.Image without a colormodel, so is more general.
