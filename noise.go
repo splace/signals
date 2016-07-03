8f0cae621da0ea0ec059a808ccddaa7dc8ba2625
simon <psiplace@netscape.net>
2016-07-03 23:23:00 +0100

    fix noise thread safety

--- noise.go
+++ noise.go
@@ -27,7 +27,7 @@ func (s Noise) property(p x) (v y) {
 	noiseMutex.Lock()
 	rand.Seed(int64(p))                   // default generator set to the same seed for the same x
 	s.generator.Seed(int64(rand.Int63())) // Noise sets its generator's seed to a random number from default generator, which is the same at a given x, so the same random numbers generated from it, for the same x, but different for different Noises.
-	v += y(s.generator.Int63())
+	v = y(s.generator.Int63())
 	v -= y(s.generator.Int63())
 	noiseMutex.Unlock()
 	return
