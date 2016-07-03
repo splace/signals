6f48db47b9ed92c3425933dc7e03f2d815f9720d
simon <psiplace@netscape.net>
2016-07-03 21:27:48 +0100

    thread safe Noise

--- noise.go
+++ noise.go
@@ -20,10 +20,14 @@ func NewNoise() Noise {
 	return Noise{*rand.New(rand.NewSource(rand.Int63()))} // give each noise, very probably, a different generator source
 }
 
+var noiseMutex = &sync.Mutex{}
+
 func (s Noise) property(p x) (v y) {
+	noiseMutex.Lock()
 	rand.Seed(int64(p))                   // default generator set to the same seed for the same x
 	s.generator.Seed(int64(rand.Int63())) // Noise sets its generator's seed to a random number from default generator, which is the same at a given x, so the same random numbers generated from it, for the same x, but different for different Noises.
 	v += y(s.generator.Int63())
 	v -= y(s.generator.Int63())
+	noiseMutex.Unlock()
 	return
 }
